09992d5aba7a8c8063510900f4626c39ab5fda16
thomas-laptop <thomas-h-nielsen@hotmail.com>
2017-11-08 10:12:02 +0100

    added swp to gitignore and added more to amqp client

--- src/amqpClient.py
+++ src/amqpClient.py
@@ -1,33 +1,43 @@
 from IClient import IClient
+from threading import Thread
 import pika
 
 class amqpClient(IClient):
 
     def __init__(self, amqp_url):
-        self._connection = None
-        self._channel = None
-        self._closing = False
-        self._consumer_tag = None
-        self._url = amqp_url
+        self.connection = None
+        self.pChannel = None
+        self.pThead = None
+        self.sChannel = None
+        self.sThread = None
+        self.params = None
+        self.url = amqp_url
 
     def connect(self):
-        self.params = pika.URLParameters(url)
+        self.params = pika.URLParameters(self.url)
         self.params.socket_timeout = 5
-        self.connect = pika.BlockingConnection(parameters=self.params)
-        self.channel = self.connect.channel() # start a channel
+        self.connection = pika.BlockingConnection(parameters=self.params)
+
+    def publishThread(self, **kwargs):
+        self.pChannel.basic_publish( **kwargs )
 
     def publish(self, **kwargs):
-        self.channel.basic_publish( **kwargs )
+        self.pChannel = self.connection.channel() # start a channel
+        self.pThread = Thread( target=self.publishThread, kwargs=kwargs )
+        self.pThread.start()
+
+    def subscribeThread(self, **kwargs):
+        pass
+        #self.pChannel.basic_publish( **kwargs )
 
     def subscribe(self, callback, queue):
-        self.channel.basic_consume(consumer_callback=calback,queue=queue,no_ack=True, exclusive=False,consumer_tag=None)  
-    def disconnect(self):
-        pass
+        #self.channel.basic_consume(consumer_callback=calback,queue=queue,no_ack=True, exclusive=False,consumer_tag=None)  
+        self.sChannel = self.connect.channel() # start a channel
+        self.sThread = Thread( target=self.subscribeThread, kwargs={'topics' : topics} )
+        self.sThread.start()
 
-    def on_message(self, channel, method, properties, body):
-        print(method.delivery_tag)
-        print(body)
-        print(channel.basic_ack(delivery_tag=method.delivery_tag))
+    def disconnect(self):
+        self.connection.close()
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.connect.close()
+        self.connection.close()
